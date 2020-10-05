c12fb38e2a29c72fef96f30c7fb9f904d3972b36
Binu42 <kbinu42@gmail.com>
2020-10-05 11:18:53 +0530

    interface and class

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
-const name = 'Binu',
-  age = 24,
-  gender = 'male';
+// interface Human {
+//   name: string;
+//   age: Number;
+//   gender: string;
+// }
 
-const sayHi = (name: string, age: Number, gender?: string): string => {
-  return `Hello ${name}, you are ${age}year old and you are ${gender}`;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender?: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const binu = new Human('Binu', 24, 'male');
+const optionalBinu = new Human('Binu', 24);
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}year old and you are ${person.gender}`;
 };
 
-console.log(sayHi(name, age, gender));
-console.log(sayHi(name, age));
+console.log(sayHi(binu));
+console.log(sayHi(optionalBinu));
 
 export {};
